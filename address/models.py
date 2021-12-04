119c8187be8644450a123343f54fe1bb5ef71d46
Saifuzzaman-Sabuj <Saifuzzaman.sabuj@gmail.com>
2021-12-05 01:08:45 +0600

    API PROJECT

--- address/models.py
+++ address/models.py
@@ -9,7 +9,7 @@ class Address(models.Model):
     line2 = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=5)
-    zip = models.IntegerField(null=True)
+    zip = models.CharField(max_length=200, null=True)
 
 
 
